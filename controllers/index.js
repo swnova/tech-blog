ee0126dff35c986678cc8064f9aae5cdca2ea6d3
swnova <sharrodwillanova@gmail.com>
2022-11-27 03:10:35 -0800

    adding routes and file structure

---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const homeRoutes = require('./homeRoutes');
+const commentRoutes = require('./api/commentRoutes');
+const userRoutes = require('./api/userRoutes');
+const postRoutes = require('./api/postRoutes');
+
+router.use('/', homeRoutes);
+router.use('/api/comments', commentRoutes)
+router.use('/api/users', userRoutes)
+router.use('/api/posts', postRoutes);
+module.exports = router;
