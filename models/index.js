92c51e10cc183f4c356e62a35ef1c244411228fc
swnova <sharrodwillanova@gmail.com>
2022-12-23 02:35:10 -0800

    redeploy att

--- models/index.js
+++ models/index.js
@@ -2,6 +2,9 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
 
 Post.belongsTo(User, {
   foreignKey: 'user_id',
